8bfcba66331aa98412bb6263656e1fc09a6a55a9
nafis ahamed <nafisahamed14@gmail.com>
2026-03-04 19:15:27 +0600

    overriding

--- Program.cs
+++ Program.cs
@@ -1,125 +1,41 @@
 ﻿using System;
 
-public interface INotify
+// polymorphism two types
+
+// compile time / static / overloading
+
+// run time / dynamic / overriding
+class Calculator()
 {
-    public void sendMessage();
-    public void log();
-    public void Save();
-}
 
-public class EmailNotify : INotify
-{
-    public string Email { get; set; }
-    public void sendMessage()
+    // overLoading void add
+    public void Add(int a, int b)
     {
-        Console.WriteLine($"Sending Email to {Email}");
+    
     }
 
-    public void log()
+
+    // method overriding is abstraction
+
+    public void Add(double a, double b)
     {
-        Console.WriteLine("Log Email");
+        
     }
-
-    public void Save()
+    public void Add(int a, int b, int c)
     {
-        Console.WriteLine("Email Save");
+        
     }
-}
-
-
-
-public class SmsNotify : INotify
-{
-    public string MobileNumber { get; set; }
-
-    public void sendMessage()
+    public void Add(int a, int b, int c, int d)
     {
-        Console.WriteLine($"Sending SMS to {MobileNumber}");
-    }
-
-    public void log()
-    {
-        Console.WriteLine("Log Email");
-    }
-
-    public void Save()
-    {
-        Console.WriteLine("Email Save");
+        
     }
 }
-
-public class PushNotify : INotify
-{
-    public string Token {get; set;}
-
-     public void sendMessage()
-    {
-        Console.WriteLine($"Sending Token to {Token}");
-    }
-
-    public void log()
-    {
-        Console.WriteLine("Log Email");
-    }
-
-    public void Save()
-    {
-        Console.WriteLine("Email Save");
-    }
-
-}
-
-public class NotifyContext
-{
-    public INotify notify { get; set; }
-
-    public NotifyContext(INotify notify)
-    {
-        this.notify = notify;
-    }
-
-    public void Process()
-    {
-        notify.sendMessage();
-        notify.log();
-        notify.Save();
-    }
-}
-
 
 class Program
 {
     static void Main(string[] args)
     {
-    
-        IList<NotifyContext> notifies = new List<NotifyContext>();
-
-        INotify emailNotify = new EmailNotify
-        {
-            Email = "nafis@gmail.com"
-        };
-
-
-
-        INotify smsNotify = new SmsNotify { MobileNumber = "01922208141" };
-
-        INotify pushNotify = new PushNotify{Token ="abcd"};
-
-
-        NotifyContext emailNotifyContext = new NotifyContext(emailNotify);
-
-        NotifyContext smsNotifyContext = new NotifyContext(smsNotify);
-
-        NotifyContext pushNotifyContext = new NotifyContext(pushNotify);
-
-        notifies.Add(emailNotifyContext);
-        notifies.Add(smsNotifyContext);
-        notifies.Add(pushNotifyContext);
-
-        foreach (var item in notifies)
-        { 
-            item.Process();  
-        }
 
     }
+
 }
--- oop_basic.md
+++ oop_basic.md
@@ -594,3 +594,39 @@ class Program
 
     }
 }
+
+
+<!-- polymorphism -->
+
+using System;
+
+// polymorphism two types
+
+// compile time / static / overloading
+
+// run time / dynamic / overriding
+class Calculator()
+{
+
+    // overLoading void add
+    public void Add(int a, int b)
+    {
+    
+    }
+
+
+    // method overriding is abstraction
+
+    public void Add(double a, double b)
+    {
+        
+    }
+    public void Add(int a, int b, int c)
+    {
+        
+    }
+    public void Add(int a, int b, int c, int d)
+    {
+        
+    }
+}
